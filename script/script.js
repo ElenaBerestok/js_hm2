4aed73b6286e56f6beb15070db625324fd8a511e
Elena Berestok <=>
2022-06-15 23:24:52 +0300

    item2

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+// ДЗ пункт 1
+
 let age;
 
 age = 23;
@@ -14,4 +16,26 @@ let userSurname;
 
 userSurname = 'Shevchenko';
 
-console.log(userSurname)
+console.log(userSurname);
+
+// ДЗ пункт 2
+
+// переменной, которая хранит нового пользователя
+
+let newUser;
+
+// переменной, которая хранит список покупок
+
+let shoppingList;
+
+// переменной, которая хранит информацию о том, является ли пользователь в сети (online)
+
+let onlineUser;
+
+// переменной, которая хранит зарплату работника
+
+let employeeSalary;
+
+// переменной, которая хранит координаты курсора (мыши)
+
+let coordinatesCursor;
