4f4aa95ec40c9c6c865323133b45a146bd5c6276
Elena Berestok <=>
2022-06-15 23:08:18 +0300

    create script

---
+++ script/script.js
@@ -0,0 +1,17 @@
+let age;
+
+age = 23;
+
+console.log(age);
+
+let userName;
+
+userName='Cristina';
+
+console.log(userName);
+
+let userSurname;
+
+userSurname = 'Shevchenko';
+
+console.log(userSurname)
